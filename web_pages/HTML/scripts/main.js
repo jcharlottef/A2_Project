0fc8779753163f3cd283440422a89e628e32a882
Anissa Abdel-Jelil <aabdeljelil@hds.harvard.edu>
2020-01-18 13:14:50 -0500

    add D3viz on homepage

--- web_pages/HTML/scripts/main.js
+++ web_pages/HTML/scripts/main.js
@@ -1,69 +1,126 @@
-/* Scripts for the D3 visualization */
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+/* Scripts for the D3 visualization 
+Putting the data in here directly because it's such a small dataset. 
+The original dataset can be found in the 
+Context-Twitter-Disinformation.CSV file*/
+var data = [
+				{
+								"name": "Russia",
+								"value": 8066,
+				},
+						{
+								"name": "Iran",
+								"value": 7869,
+				},
+						{
+								"name": "Bangladesh",
+								"value": 15,
+				},
+						{
+								"name": "Venezuela",
+								"value": 1993,
+				},
+						{
+								"name": "Catalonia",
+								"value": 130,
+				},
+						{
+								"name": "China",
+								"value": 5241,
+				},
+						{
+								"name": "Saudi Arabia",
+								"value": 5935,
+				},
+						{
+								"name": "Ecuador",
+								"value": 1019,
+				},
+						 {
+								"name": "United Arab Emirates",
+								"value": 4248,
+				},
+					 {
+								"name": "Spain",
+								"value": 259,
+				},
+				 {
+								"name": "Egypt or UAE",
+								"value": 271,
+				}];
 
-// Parse the date / time
-var	parseDate = d3.time.format("%d-%7").parse;
+//sort bars based on value
+			data = data.sort(function (a, b) {
+						return d3.ascending(a.value, b.value);
+				})
 
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+//set up svg using margin conventions, leaving room on the left for labels 
+				var margin = {
+						top: 15,
+						right: 125,
+						bottom: 15,
+						left: 160
+				};
 
-var y = d3.scale.linear().range([height, 0]);
+				 var width = 800 - margin.left - margin.right,
+						height = 500 - margin.top - margin.bottom;
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(d3.time.format("%B-%Y"));
+				var svg = d3.select("#graphic").append("svg")
+						.attr("width", width + margin.left + margin.right)
+						.attr("height", height + margin.top + margin.bottom)
+						.append("g")
+						.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+				var x = d3.scale.linear()
+						.range([0, width])
+						.domain([0, d3.max(data, function (d) {
+								return d.value;
+						})]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", 
-          "translate(" + margin.left + "," + margin.top + ")");
+				var y = d3.scale.ordinal()
+						.rangeRoundBands([height, 0], .1)
+						.domain(data.map(function (d) {
+								return d.name;
+						}));
 
-d3.csv("Context-Twitter-Disinformation.csv", function(error, data) {
+				//make y axis to show bar names
+				var yAxis = d3.svg.axis()
+						.scale(y)
+						//no tick marks
+						.tickSize(0)
+						.orient("left");
 
-    data.forEach(function(d) {
-        d.date = parseDate(d.date);
-        d.value = +d.value;
-    });
-	
-  x.domain(data.map(function(d) { return d.date; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+				var gy = svg.append("g")
+						.attr("class", "y axis")
+						.call(yAxis)
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
+				var bars = svg.selectAll(".bar")
+						.data(data)
+						.enter()
+						.append("g")
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Value ($)");
+				//append rects
+				bars.append("rect")
+						.attr("class", "bar")
+						.attr("y", function (d) {
+								return y(d.name);
+						})
+						.attr("height", y.rangeBand())
+						.attr("x", 0)
+						.attr("width", function (d) {
+								return x(d.value);
+						});
 
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .style("fill", "steelblue")
-      .attr("x", function(d) { return x(d.date); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); });
-
-});
+				//add a value label to the right of each bar
+				bars.append("text")
+						.attr("class", "label")
+						//y position of the label is halfway down the bar
+						.attr("y", function (d) {
+								return y(d.name) + y.rangeBand() / 2 + 4;
+						})
+						//x position is 3 pixels to the right of the bar
+						.attr("x", function (d) {
+								return x(d.value) + 3;
+						})
+						.text(function (d) {
+								return d.value;
+						});
